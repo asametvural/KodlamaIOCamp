16fa14a69a261e359f659f6580019baa5ee29289
Samet Vural <vuralasamet@gmail.com>
2021-02-12 23:55:20 +0300

    10.02.21

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç İçin Temel Kurs", "Java", "Python", "Javascript", "C++", "C#" };
+
+            Console.WriteLine("For Döngüsü");
+            Console.WriteLine("------------------------");
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + kurslar[i]);
+                Console.WriteLine("------------------------");
+            }
+
+            Console.WriteLine("ForEach Döngüsü");
+            Console.WriteLine("------------------------");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+                Console.WriteLine("------------------------");
+            }
+        }
+    }
+}

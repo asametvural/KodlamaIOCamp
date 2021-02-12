620e75deb79654fd9c167801013de67cd11d1fce
Samet Vural <vuralasamet@gmail.com>
2021-02-13 00:12:28 +0300

    10.02.21

---
+++ OOP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { smsLoggerService, fileLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(ihtiyacKrediManager);
+            //basvuruManager.BasvuruYap(tasitKrediManager,fileLoggerService);
+            //basvuruManager.BasvuruYap(konutKrediManager,databaseLoggerService);
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, konutKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}

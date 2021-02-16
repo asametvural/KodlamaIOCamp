661be021209c32eeba0ffd654365d13f16d24555
Samet Vural <vuralasamet@gmail.com>
2021-02-16 16:23:39 +0300

    Day6

---
+++ Day6Hw2.sql
@@ -0,0 +1,6 @@
+﻿select Products.ProductName as [Ürün Adı],
+sum([Order Details].UnitPrice*[Order Details].Quantity) as [Kazanılan Toplam Miktar]
+from Products
+INNER JOIN [Order Details] ON Products.ProductID = [Order Details].ProductID
+INNER JOIN Orders ON Orders.OrderID = [Order Details].OrderID
+group by Products.ProductName 
